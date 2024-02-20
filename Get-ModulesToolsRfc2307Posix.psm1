3af1e14ad3a94408f52f1f2d1b901be00bdc27cc
Alan Alves <42603277+treinalinux@users.noreply.github.com>
2024-02-20 05:27:11 -0300

    feat: Create Get-ModulesToolsRfc2307Posix.psm1

---
+++ Get-ModulesToolsRfc2307Posix.psm1
@@ -0,0 +1,96 @@
+$PortalAdmin = "C:\PortalAdmin"
+$FileLogGroups = "$PortalAdmin\group-changes.log"
+$Analyst = "Alan"
+
+function Add-UsersInGroupsPosix {
+    
+    param (
+        # Recebe o número do chamado.
+        [Parameter(Mandatory=$true)]
+        [string] $RequestNumber,
+
+        [Parameter(Mandatory=$true)]
+        [string] $OwnerGroup,
+
+        # Recebe o nome de um ou mais grupos.
+        [Parameter(Mandatory=$true)]
+        [string[]] $GroupNames,
+
+        # Recebe o nome de um ou mais usuários.
+        [Parameter(Mandatory=$true)]
+        [string[]] $UserNames
+    )
+
+    begin {
+        $AccountsValidade = @()
+        $AccountsInvalids = @()
+        $UserNames | ForEach-Object {
+            $UserEnabled = Get-UsersEnabled $_
+            $AccountsValidade += $($UserEnabled)
+        }
+
+        Start-Sleep 2
+    }
+
+    process {
+        foreach ($UserName in $AccountsValidade) {
+            try {
+                Write-Host "$($RequestNumber) - A conta de $($UserName) foi adicionado no grupo $($GroupNames) com sucesso." -ForegroundColor Green
+                $LogAcction = "$(Get-Date);$($RequestNumber);$($Analyst);$($OwnerGroup);A conta de $($UserName) foi adicionado no grupo $($GroupNames) com sucesso."
+            }
+            catch {
+                Write-Host "$($RequestNumber) - Error ao adicionar a conta de $($UserName) no grupo $($GroupNames)!" -ForegroundColor Yellow
+                $LogAcction = "$(Get-Date);$($RequestNumber);$($Analyst);$($OwnerGroup);Error ao adicionar a conta de $($UserName) no grupo $($GroupNames)!"
+            }
+            finally {
+                Write-Output "$($LogAcction)" | Out-File -Append -Encoding utf8 -FilePath $FileLogGroups
+            }
+        }
+    }
+
+    end {
+        $UserNames | ForEach-Object {
+            $InvalidUser = Get-UsersEnabled $_
+            $AccountsInvalids += $($InvalidUser)
+        }
+    }
+}
+
+
+function Get-UsersEnabled {
+    
+    param (
+        # Recebe o nome de um ou mais usuários.
+        [Parameter(Mandatory=$true)]
+        [string[]] $UserNames
+    )
+
+    $UserNames | ForEach-Object {
+        (Get-ADUser -Identity $_).Enabled
+        if ($($_.Enabled) -eq "Enabled") {
+            $UserEnabled =  $($_.SamAccountName)
+            return $UserEnabled.tolower()
+        }
+    }
+}
+
+function Get-InvalidUsers {
+    
+    param (
+        # Recebe o nome de um ou mais usuários.
+        [Parameter(Mandatory=$true)]
+        [string[]] $UserNames
+    )
+
+    $UserNames | ForEach-Object {
+        try {
+            $UserName = Get-ADUser -Identity $_ -Propreties DisplayName
+            if ($($UserName.Enabled) -eq "Disabled") {
+                Write-host " A conta $($UserName.SamAccountName) de $($UserName.DisplayName) está desativada no AD!" -ForegroundColor Yellow
+            }
+        }
+        catch {
+            Write-Host " A $($_) conta informada não existe na base de dados do AD!" -ForegroundColor Red
+        }
+    }
+}
